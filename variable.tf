f5e3a3d0c4cf189dcc531b67510c3729761f8f13
mtulasi41 <116076880+mtulasi41@users.noreply.github.com>
2023-10-17 16:15:16 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,7 +8,9 @@ variable "subnet_cidr" {
 }
 variable "ami_id" {
     type = string
+    default = "ami-0f5ee92e2d63afc18"
 }
 variable "instance_type" {
     type = string
+    default = "t2.micro"
 }
