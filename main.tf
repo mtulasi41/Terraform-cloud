8a863839055f983c1de337c349626ce88a7aaeb7
mtulasi41 <116076880+mtulasi41@users.noreply.github.com>
2023-10-17 21:02:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webserver"{
         ami = var.ami_id
         instance_type = var.instance_type
-        subnet_id = var.subnet_cidr
+        subnet_id = var.subnet_id
         tags = {
           Name = "Terraform-cloud"
         }
