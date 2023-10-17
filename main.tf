f083bf81fe9e50896e4b5c928176f080db14d2c5
mtulasi41 <116076880+mtulasi41@users.noreply.github.com>
2023-10-17 16:17:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr
+}
+resource "aws_subnet" "my_subnet" {
+    vpc_id = var.vpc_id
+    cidr_block = var.subnet_cidr
+}
 resource "aws_instance" "webserver"{
         ami = var.ami_id
         instance_type = var.instance_type
